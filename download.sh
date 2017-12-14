84d37a64378652b0103ec87637176506efd04796
Vincent Sellier <vsellier@exoplatform.com>
2017-12-15 00:03:04 +0100

    Download the version at the right place

--- download.sh
+++ download.sh
@@ -5,8 +5,10 @@ echo "Downloading chat server version ${CHAT_SERVER_VERSION}"
 REPO="exo"
 if [[ "${CHAT_SERVER_VERSION}" =~ "SNAPSHOT" ]]; then
     REPO="n/exo-addons-snapshots"
+elif [[ "${CHAT_SERVER_VERSION}" =~ "-RC" ]] || [[ "${CHAT_SERVER_VERSION}" =~ "-M" ]]; then
+    REPO="n/exo-addons-releases"
 fi
 
-DOWNLOAD_URL=http://addons.exoplatform.org/${REPO}/${GROUP_ID}/${ARTIFACT_ID}/${VERSION}/zip
+DOWNLOAD_URL=http://addons.exoplatform.org/${REPO}/${GROUP_ID}/${ARTIFACT_ID}/${CHAT_SERVER_VERSION}/zip
 
 wget -O chatserver.zip ${DOWNLOAD_URL}
