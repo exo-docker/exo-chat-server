9795758803e93e8bd0b228a4624ff59d071924a8
Frédéric DROUET <fdrouet@exoplatform.com>
2018-04-03 17:04:22 +0200

    DOCKER-37 : fix the download script

--- download.sh
+++ download.sh
@@ -2,11 +2,13 @@
 
 echo "Downloading chat server version ${CHAT_SERVER_VERSION}"
 
-REPO="exo"
 if [[ "${CHAT_SERVER_VERSION}" =~ "SNAPSHOT" ]]; then
     REPO="n/exo-addons-snapshots"
 elif [[ "${CHAT_SERVER_VERSION}" =~ "-RC" ]] || [[ "${CHAT_SERVER_VERSION}" =~ "-M" ]]; then
     REPO="n/exo-addons-releases"
+else
+    REPO="exo"
+    GROUP_ID="chat-server-standalone"
 fi
 
 DOWNLOAD_URL=http://addons.exoplatform.org/${REPO}/${GROUP_ID}/${ARTIFACT_ID}/${CHAT_SERVER_VERSION}/zip
