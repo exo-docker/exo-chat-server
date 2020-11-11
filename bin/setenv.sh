996d2a7c3a67f1d60d8e759fea10690785b6f1f7
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-11-11 20:16:16 +0100

    Allow to define http port of chat server

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -123,6 +123,7 @@ else
   # Tomcat HTTP Thread pool configuration
   xmlstarlet ed -L -s "/Server/Service/Connector" -t attr -n "maxThreads" -v "${EXO_HTTP_THREAD_MAX}" \
     -s "/Server/Service/Connector" -t attr -n "minSpareThreads" -v "${EXO_HTTP_THREAD_MIN}" \
+    -u "/Server/Service/Connector/@port" -v "${CHAT_HTTP_PORT:8080}" \
     ${CHAT_APP_DIR}/conf/server.xml || {
     echo "ERROR during xmlstarlet processing (adding Connector proxyName)"
     exit 1
@@ -130,17 +131,6 @@ else
 
   # JMX configuration
   if [ "${EXO_JMX_ENABLED}" = "true" ]; then
-    # insert the listener before the "Global JNDI resources" line
-    xmlstarlet ed -L -i "/Server/GlobalNamingResources" -t elem -n ListenerTMP -v "" \
-      -i "//ListenerTMP" -t attr -n "className" -v "org.apache.catalina.mbeans.JmxRemoteLifecycleListener" \
-      -i "//ListenerTMP" -t attr -n "rmiRegistryPortPlatform" -v "${EXO_JMX_RMI_REGISTRY_PORT}" \
-      -i "//ListenerTMP" -t attr -n "rmiServerPortPlatform" -v "${EXO_JMX_RMI_SERVER_PORT}" \
-      -i "//ListenerTMP" -t attr -n "useLocalPorts" -v "false" \
-      -r "//ListenerTMP" -v "Listener" \
-      ${CHAT_APP_DIR}/conf/server.xml || {
-      echo "ERROR during xmlstarlet processing (adding JmxRemoteLifecycleListener)"
-      exit 1
-    }
     # Create the security files if required
     if [ "${EXO_JMX_USERNAME:-}" != "-" ]; then
       if [ "${EXO_JMX_PASSWORD:-}" = "-" ]; then
