591b9d5e4470ef66067f7cbf0245f067be8c46c1
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-11-18 09:21:30 +0100

    Allow to define http port of chat server (#5)

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -30,6 +30,8 @@ set +u		# DEACTIVATE unbound variable check
 [ -z "${CHAT_MONGO_DB_AUTHENTICATION}" ] && CHAT_MONGO_DB_AUTHENTICATION="false"
 [ -z "${CHAT_MONGO_DB_USER}" ] && CHAT_MONGO_DB_USER=""
 [ -z "${CHAT_MONGO_DB_PASSWORD}" ] && CHAT_MONGO_DB_PASSWORD=""
+[ -z "${CHAT_HTTP_PORT}" ] && CHAT_HTTP_PORT="8080"
+[ -z "${CHAT_SERVER_STOP_PORT}" ] && CHAT_SERVER_STOP_PORT="8005"
 [ -z "${CHAT_PORTAL_PAGE}" ] && CHAT_PORTAL_PAGE="/portal/intranet/chat"
 [ -z "${CHAT_PASSPHRASE}" ] && CHAT_PASSPHRASE="changeme"
 [ -z "${CHAT_CRON_NOTIF_CLEANUP}" ] && CHAT_CRON_NOTIF_CLEANUP="0 0/60 * * * ?"
@@ -124,7 +126,14 @@ else
   xmlstarlet ed -L -s "/Server/Service/Connector" -t attr -n "maxThreads" -v "${EXO_HTTP_THREAD_MAX}" \
     -s "/Server/Service/Connector" -t attr -n "minSpareThreads" -v "${EXO_HTTP_THREAD_MIN}" \
     ${CHAT_APP_DIR}/conf/server.xml || {
-    echo "ERROR during xmlstarlet processing (adding Connector proxyName)"
+    echo "ERROR during xmlstarlet processing (adding Connector thread pool configuration)"
+    exit 1
+  }
+
+  # Tomcat HTTP port configuration
+  xmlstarlet ed -L -u '/Server/Service/Connector/@port' -v "${CHAT_HTTP_PORT}" \
+    -u "/Server/@port" -v "${CHAT_SERVER_STOP_PORT}" ${CHAT_APP_DIR}/conf/server.xml || {
+    echo "ERROR during xmlstarlet processing (changing Connector port)"
     exit 1
   }
 
